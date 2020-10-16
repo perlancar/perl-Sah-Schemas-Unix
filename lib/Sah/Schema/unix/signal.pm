5c0001f882ad7c4e868998c60a132e63e02463eb
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-10-16 08:08:13 +0700

    [schema unix::signal] Change x.examples attribute to examples property

--- lib/Sah/Schema/unix/signal.pm
+++ lib/Sah/Schema/unix/signal.pm
@@ -8,7 +8,7 @@ package Sah::Schema::unix::signal;
 our $schema = ['str' => {
     'summary' => 'Unix signal name (e.g. TERM or KILL) or number (9 or 15)',
     match => '\A(?:[A-Z]+[0-9]*|[1-9][0-9]*)\z',
-    'x.examples' => [qw/HUP INT QUIT ILL ABRT FPE KILL SEGV PIPE ALRM TERM USR1 USR2 CHLD CONT STOP TSTP TTIN TTOU/, 1..15],
+    examples => [qw/HUP INT QUIT ILL ABRT FPE KILL SEGV PIPE ALRM TERM USR1 USR2 CHLD CONT STOP TSTP TTIN TTOU/, 1..15],
 _
 }, {}];
 
