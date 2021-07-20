cb7029a4e06700db2097770ac2800dad46065b51
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-20 08:44:14 +0700

    Re-add valid examples in unix::signal schema

--- lib/Sah/Schema/unix/signal.pm
+++ lib/Sah/Schema/unix/signal.pm
@@ -9,9 +9,12 @@ our $schema = ['str' => {
     'summary' => 'Unix signal name (e.g. TERM or KILL) or number (9 or 15)',
     match => '\A(?:[A-Z]+[0-9]*|[1-9][0-9]*)\z',
     examples => [
+        # valid signals found in most Unix systems
+        qw/HUP INT QUIT ILL ABRT FPE KILL SEGV PIPE ALRM TERM USR1 USR2 CHLD CONT STOP TSTP TTIN TTOU/,
+        1..15,
+
+        # other tests
         {data=>'', valid=>0},
-        {data=>'HUP', valid=>1},
-        {data=>'1', valid=>1},
     ],
 _
 }, {}];
